a2b10841f7088c42751750090f2a1c621078ea3a
Priya Papechen <111747255+PPTaniya@users.noreply.github.com>
2026-06-02 14:23:00 +0530

    Add ITransport interface for IPC transport

---
+++ include/ipc/transport/itransport.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ipc/common/message.hpp"
+
+namespace ipc
+{
+
+class ITransport
+{
+public:
+
+    virtual bool send(const Message& msg) = 0;
+
+    virtual bool receive(Message& msg) = 0;
+
+    virtual int getFd() const = 0;
+
+    virtual ~ITransport() = default;
+};
+
+}
